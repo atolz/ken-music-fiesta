fb87e96140be1a384de9ff49e948ae43458265e7
Atolagbe Elisha <atolagbeelisha001@gmail.com>
2022-05-07 22:49:43 +0100

    added-profile-page

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -60,6 +60,7 @@ export default function Home() {
       <RaffleTickets className={` transition-all ease-in ${activePage == "Raffle Tickets" ? "visible opacity-100" : " invisible opacity-0 h-0"}`}></RaffleTickets>
       <Rewards className={` transition-all ease-in ${activePage == "Rewards" ? "visible opacity-100" : " invisible opacity-0 h-0"}`}></Rewards>
       <LiveStream className={` transition-all ease-in ${activePage == "Livestream Event" ? "visible opacity-100" : " invisible opacity-0 h-0"}`}></LiveStream>
+      <Profile className={` transition-all ease-in ${activePage == "Profile" ? "visible opacity-100" : " invisible opacity-0 h-0"}`}></Profile>
       {/* {activePage == "Raffle Tickets" && <RaffleTickets></RaffleTickets>}
       {activePage == "Rewards" && <Rewards></Rewards>}
       {activePage == "Livestream Event" && <LiveStream></LiveStream>}
