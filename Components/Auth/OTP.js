ddc5f2954b140022d5d54269835cb279013b76bb
Atolagbe Elisha <atolagbeelisha001@gmail.com>
2022-05-10 13:45:54 +0100

    otp-ux-fix

--- Components/Auth/OTP.js
+++ Components/Auth/OTP.js
@@ -6,10 +6,12 @@ import { setLoginStatus } from "../../store/user";
 import { baseInstanceAPI } from "../../axios";
 import useLoading from "../../hooks/useLoading";
 import useShowAlert from "../../hooks/useShowAlert";
+import OtpInput from "react-otp-input";
 
 const OTP = ({ action }) => {
   const inputAmount = [1, 2, 3, 4, 5, 6];
   const { isLoading, toggleLoad } = useLoading();
+  const [otpVal, setOtpVal] = useState("");
   const [otp, setOtp] = useState([]);
   const [uuid, setUuid] = useState("");
   const [showStatus, setShowStatus] = useState(false);
@@ -29,7 +31,7 @@ const OTP = ({ action }) => {
   }, [router.isReady]);
 
   const onVerify = async () => {
-    const data = { uuid: uuid, otp: otp.toString().replace(/,/g, "") };
+    const data = { uuid: uuid, otp: otpVal.toString().replace(/,/g, "") };
     console.log("data is ", JSON.stringify(data));
     toggleLoad();
     try {
@@ -49,12 +51,17 @@ const OTP = ({ action }) => {
     toggleLoad();
   };
 
+  const handleOtpChange = (otp) => {
+    setError("");
+    setOtpVal(otp);
+  };
+
   return (
     <>
       {!showStatus && (
         <div className="auth-container !mb-[10rem]">
           <form
-            className="auth-form"
+            className="verify-input"
             onSubmit={(e) => {
               e.preventDefault();
               onVerify();
@@ -66,7 +73,7 @@ const OTP = ({ action }) => {
             <h3>OTP Verification</h3>
             <p className="mb-[3.6rem]">Enter the code that was sent to your mail to continue registration.</p>
             {error && <p className=" !text-[1.4rem] !text-red-500">*{error}</p>}
-            <div className="grid grid-cols-6 gap-5 gap-y-[2.4rem]">
+            {/* <div className="grid grid-cols-6 gap-5 gap-y-[2.4rem]">
               {inputAmount.map((el, i) => {
                 return (
                   <div key={el} className="form-group ">
@@ -89,7 +96,33 @@ const OTP = ({ action }) => {
               })}
 
               <button className="btn col-span-6 mt-[6.8rem]">Verify OTP</button>
-            </div>
+            </div> */}
+            <OtpInput
+              inputStyle={{
+                background: "white",
+                width: "100%",
+                height: "70px",
+                fontFamily: "Montserrat",
+                fontSize: "16px",
+                "border-radius": "1rem",
+                "@media (max-width: 600px)": {
+                  height: "40px",
+                },
+              }}
+              focusStyle={{
+                outline: "2px solid #fcac0d",
+              }}
+              hasErrored={!!error}
+              errorStyle={{
+                outline: "1px solid red",
+              }}
+              shouldAutoFocus={true}
+              value={otpVal}
+              onChange={handleOtpChange}
+              numInputs={6}
+              separator={<span>-</span>}
+            />
+            <button className="btn w-full mt-[6.8rem]">Verify OTP</button>
           </form>
         </div>
       )}
