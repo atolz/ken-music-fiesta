8a66bb021fe7d05b4d850b175bff2262ea67f8e7
Atolagbe Elisha <atolagbeelisha001@gmail.com>
2022-06-10 11:58:02 +0100

    live-stream-v1

--- Components/Pages/ProgressiveToken.js
+++ Components/Pages/ProgressiveToken.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import ReactPlayer from "react-player";
 import { popUpContext } from "../../Context/PopUps";
 
 const ProgressiveToken = () => {
@@ -6,9 +7,13 @@ const ProgressiveToken = () => {
   return (
     <section
       //   style={{ backgroundSize: "100%" }}
-      className="px-[2rem] bg-[#FFF6E4] p-[4.6rem] bg-cover bg-center bg-[url(/performing.jpg)] bg-no-repeat w-full h-[70vh] mobile:h-[70vh] md:!h-[79vh] rounded-[2rem] grid place-items-end "
+      className="px-[2rem] overflow-hidden relative bg-[#FFF6E4] p-[6.6rem] bg-cover bg-center bg-[url(/performing.jpg)] bg-no-repeat w-full h-[70vh] mobile:h-[70vh] md:!h-[79vh] rounded-[2rem] grid place-items-end "
     >
-      <div className=" py-[3.5rem] px-[5.8rem] rounded-[2rem] bg-white flex flex-wrap gap-[4rem] justify-between mx-auto">
+      <div className=" absolute top-0 left-0 w-full h-full">
+        <ReactPlayer width={"100%"} height={"100%"} light={"/performing.jpg"} controls={true} url="https://www.youtube.com/watch?v=oKKKDfkgBho" />
+      </div>
+      {/* <ReactPlayer controls={true} url="https://www.youtube.com/watch?v=xTI5BW_jZXY" /> */}
+      <div className=" py-[1.8rem] px-[3.8rem] rounded-[2rem] bg-white flex flex-wrap gap-[4rem] justify-between mx-auto relative items-center">
         <p className=" font-bold text-[1.8rem] leading-[3.5rem] max-w-[34.9rem]">
           Participate in our quick raffle draw and win <span className="text-[#FCAC0D]">amazing prices!</span>
         </p>
