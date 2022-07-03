2c2a781cdfc6019466b640e1de912dc3e0ed8ca4
Atolagbe Elisha <atolagbeelisha001@gmail.com>
2022-07-03 23:28:53 +0100

    preload:display-swap

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ export default function Document() {
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,800&display=optional" rel="preload" as="font" type="font/woff2" />
+        <link rel="preload" as="font" type="font/woff2" href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,800&display=swap" />
       </Head>
       <body>
         <Main />
