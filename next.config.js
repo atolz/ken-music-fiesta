9d9565d44ff65ebf0811e05cd1e04c793472653a
Atolagbe Elisha <atolagbeelisha001@gmail.com>
2022-07-04 00:11:25 +0100

    remove-next-js-font-optimaization

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  optimizeFonts: false,
   reactStrictMode: true,
   images: {
     domains: ["kennis-bucket.s3.eu-central-1.amazonaws.com", "cdn.kennismusic.app"],
