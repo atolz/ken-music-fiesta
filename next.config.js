0051b66391b925ae7bb7dfa76b4533f03e38045f
Atolagbe Elisha <atolagbeelisha001@gmail.com>
2022-07-04 00:58:49 +0100

    next-googlefont-opt

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  optimizeFonts: false,
+  // optimizeFonts: false,
   reactStrictMode: true,
   images: {
     domains: ["kennis-bucket.s3.eu-central-1.amazonaws.com", "cdn.kennismusic.app"],
--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,11 @@ export default function Document() {
         {/* Prevent Layout Shifting due/Layout Janking to FOUT and FOIT **:rel="preload" display=optional */}
         {/* <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=optional" rel="preload" as="font" /> */}
 
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,800&display=swap" rel="preload stylesheet" as="font" type="font/woff2" />
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin /> */}
+
+        {/* <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,800&display=swap" type="font/woff2" as="font" rel="preload" /> */}
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,800&display=swap" rel="stylesheet" />
 
         {/* Next downloads the font at build time */}
         {/* <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,800&display=optional" rel="stylesheet" /> */}
